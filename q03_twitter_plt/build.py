4a81598bbf86ac0ab77060ab5569adb34fe24d34
Nikhil Nair <nnair25@gmail.com>
2018-08-08 10:21:26 +0530

    Updated twitter project

---
+++ q03_twitter_plt/build.py
@@ -0,0 +1,8 @@
+# Importing modules and packages
+
+import matplotlib.pyplot as plt
+import pandas as pd
+plt.switch_backend('agg')
+%matplotlib inline
+from greyatomlib.twitter_sentiment_analysis_new.q02_tweet_info.build import q02_tweet_info
+
